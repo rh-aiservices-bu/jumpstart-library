162f0ec397eb92273f31346b5e187e6686bd8a2d
ksingh7 <karan.singh731987@gmail.com>
2021-06-02 19:18:37 +0530

    fixes in seed_database service app.py

--- pattern2-smart-city/source/SC_Seed_Database/app.py
+++ pattern2-smart-city/source/SC_Seed_Database/app.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 import json, urllib.request
+import os, sys
 
 JSON_DB_FILE = "vehicle_metadata_db.json"
 
@@ -31,7 +32,7 @@ class Create_Table(Base):
     vehicle_owner_zip_code = Column(String)
     vehicle_owner_contact_number = Column(String)
     customer_id = Column(String)
-    customer_balance = Column(String)
+    customer_balance = Column(Integer)
     customer_name = Column(String)
     customer_address = Column(String)
     customer_city = Column(String)
@@ -42,15 +43,14 @@ class Create_Table(Base):
 ## Create Table if does not exists
 Create_Table.__table__.create(bind=engine, checkfirst=True)
 
-with urllib.request.urlopen(JSON_DB_FILE) as url:
-    data = json.loads(url.read().decode())
+with open(JSON_DB_FILE) as db_source:
+    data = json.load(db_source)
 
 connection = engine.connect()
 
 for count in range(len(data)):
     try:
-        connection.execute(f"""INSERT INTO public.{TABLE_NAME}(vehicle_registered_plate_number,vehicle_color,vehicle_make,vehicle_body_type,vehicle_make_model,vehicle_model_year,vehicle_registered_city,vehicle_owner_name,vehicle_owner_address,vehicle_owner_city,vehicle_owner_zip_code,vehicle_owner_contact_number,customer_id,customer_balance,customer_name,customer_address,customer_city,customer_zip_code,customer_contact_number) 
-VALUES('{data[count]['vehicle_registered_plate_number']}', '{data[count]['vehicle_color']}', '{data[count]['vehicle_make']}', '{data[count]['vehicle_body_type']}', '{data[count]['vehicle_make_model']}', '{data[count]['vehicle_model_year']}', '{data[count]['vehicle_registered_city']}', '{data[count]['vehicle_owner_name']}', '{data[count]['vehicle_owner_address']}', '{data[count]['vehicle_owner_city']}', '{data[count]['vehicle_owner_zip_code']}', '{data[count]['vehicle_owner_contact_number']}', '{data[count]['customer_id']}', '{data[count]['customer_balance']}', '{data[count]['customer_name']}', '{data[count]['customer_address']}', '{data[count]['customer_city']}', '{data[count]['customer_zip_code']}', '{data[count]['customer_contact_number']}')""")
+        connection.execute(f"""INSERT INTO public.{TABLE_NAME}(vehicle_registered_plate_number,vehicle_color,vehicle_make,vehicle_body_type,vehicle_make_model,vehicle_model_year,vehicle_registered_city,vehicle_owner_name,vehicle_owner_address,vehicle_owner_city,vehicle_owner_zip_code,vehicle_owner_contact_number,customer_id,customer_balance,customer_name,customer_address,customer_city,customer_zip_code,customer_contact_number,metadata_image_name) VALUES('{data[count]['vehicle_registered_plate_number']}', '{data[count]['vehicle_color']}', '{data[count]['vehicle_make']}', '{data[count]['vehicle_body_type']}', '{data[count]['vehicle_make_model']}', '{data[count]['vehicle_model_year']}', '{data[count]['vehicle_registered_city']}', '{data[count]['vehicle_owner_name']}', '{data[count]['vehicle_owner_address']}', '{data[count]['vehicle_owner_city']}', '{data[count]['vehicle_owner_zip_code']}', '{data[count]['vehicle_owner_contact_number']}', '{data[count]['customer_id']}', '{data[count]['customer_balance']}', '{data[count]['customer_name']}', '{data[count]['customer_address']}', '{data[count]['customer_city']}', '{data[count]['customer_zip_code']}', '{data[count]['customer_contact_number']}', '{data[count]['metadata_image_name']}')""")
     except:
         print("====================== Error inserting record to database ======================")
         print(sys.exc_info()[1])
