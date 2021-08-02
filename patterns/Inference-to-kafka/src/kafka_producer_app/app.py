75e2a2c6713a75e75fbabe6834a12928ccf58c8d
ksingh7 <karan.singh731987@gmail.com>
2021-08-02 18:43:32 +0530

    fixes to pattern inference-to-kafka

--- patterns/Inference-to-kafka/src/kafka_producer_app/app.py
+++ patterns/Inference-to-kafka/src/kafka_producer_app/app.py
@@ -1,13 +1,12 @@
 import asyncio, os, time
 from aiokafka import AIOKafkaProducer
 
-## global variable :: setting this for kafka producer
+## Global Variables to define Kafka Endpoint and Kafka Topic
 KAFKA_ENDPOINT = os.getenv('KAFKA_ENDPOINT', 'localhost:9092')
 KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'lpr')
 
 async def main():
     ## kafka producer initialization
-    #kafkaproducer = AIOKafkaProducer(loop=asyncio.get_event_loop(), bootstrap_servers=KAFKA_ENDPOINT)
     kafkaproducer = AIOKafkaProducer(bootstrap_servers=KAFKA_ENDPOINT)
     print("------ Starting Kafka Producer ------")
     await kafkaproducer.start()
@@ -17,7 +16,7 @@ async def main():
         time.sleep(5)
         ### Your buiness logic Ends here ###
         try:
-            ## Sending message to Kafka Topic and wait for response
+            ## Sending message to Kafka Topic
             response = await kafkaproducer.send_and_wait(KAFKA_TOPIC, message.encode('utf-8'))
             await asyncio.sleep(1)
             print("[producer] Message successfully written to Kafka Topic ...")
@@ -26,9 +25,6 @@ async def main():
             print("------------------------------------------------")
         except asyncio.CancelledError:
             print("[producer] cancelled...")
-        # finally:
-        #     await kafkaproducer.stop()
-        #     print("------ Kafka Producer Stopped ------")
 
 if __name__ == "__main__":
     asyncio.run(main())
