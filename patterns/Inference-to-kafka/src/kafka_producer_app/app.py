3e03d732084b0be52b8992dff27b554f053326b0
ksingh7 <karan.singh731987@gmail.com>
2021-08-02 18:33:20 +0530

    fixes to pattern inference-to-kafka

--- patterns/Inference-to-kafka/src/kafka_producer_app/app.py
+++ patterns/Inference-to-kafka/src/kafka_producer_app/app.py
@@ -7,6 +7,7 @@ KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'lpr')
 
 async def main():
     ## kafka producer initialization
+    #kafkaproducer = AIOKafkaProducer(loop=asyncio.get_event_loop(), bootstrap_servers=KAFKA_ENDPOINT)
     kafkaproducer = AIOKafkaProducer(loop=asyncio.get_event_loop(), bootstrap_servers=KAFKA_ENDPOINT)
     print("------ Starting Kafka Producer ------")
     await kafkaproducer.start()
@@ -18,15 +19,16 @@ async def main():
         try:
             ## Sending message to Kafka Topic and wait for response
             response = await kafkaproducer.send_and_wait(KAFKA_TOPIC, message.encode('utf-8'))
+            await asyncio.sleep(1)
             print("[producer] Message successfully written to Kafka Topic ...")
             print("[producer] Printing RecordMetadata ...")
             print(response)
-            print("------------------------------------------------------------------------")
+            print("------------------------------------------------")
         except asyncio.CancelledError:
             print("[producer] cancelled...")
-        finally:
-            await kafkaproducer.stop()
-            print("------ Kafka Producer Stopped ------")
+        # finally:
+        #     await kafkaproducer.stop()
+        #     print("------ Kafka Producer Stopped ------")
 
 if __name__ == "__main__":
     asyncio.run(main())
